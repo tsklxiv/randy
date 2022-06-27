dd25dac949e9373d1aef8e1f28527fb3e8414880
Dang Hoang Tuan <danghoangtuan526@proton.me>
2022-06-27 20:52:14 +0700

    Replace tracing with log + enable pretty env logger

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,8 @@ Tools:
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+
     // GET /rand/<min>/<max>
     let rand = warp::path!("rand" / u16 / u16)
         .map(|min, max| random(min, max));
