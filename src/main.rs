21f726b2c9c69c23fca77d546cb6d687fa55900f
Dang Hoang Tuan <danghoangtuan526@proton.me>
2022-06-27 14:30:28 +0700

    Add index path for help with services

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,19 @@ fn random(min: u16, max: u16) -> String {
     format!("{}", rand::thread_rng().gen_range(min..max))
 }
 
+fn index() -> String {
+    format!("Services:\nRNG: /rand/<min>/<max>")
+}
+
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    // GET /rand/<min>/<max>
     let rand = warp::path!("rand" / u16 / u16)
         .map(|min, max| random(min, max));
+    let index = warp::path!("/")
+        .map(index);
 
-    warp::serve(rand)
+    warp::serve(index)
         .run(([0, 0, 0, 0], 8000))
         .await;
 }
