c869a7ec7200107c8de3c7df54ea056cbb8de585
Dang Hoang Tuan <danghoangtuan526@proton.me>
2022-06-27 14:42:10 +0700

    Add path /now/<utc/local> for returning current time

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use warp::Filter;
 use rand::Rng;
+use chrono::prelude::*;
 
 fn random(min: u16, max: u16) -> String {
     format!("{}", rand::thread_rng().gen_range(min..max))
 }
 
+fn right_now(mode: String) -> String {
+    if mode == "utc" {
+        format!("{}", Utc::now())
+    } else if mode == "local" {
+        format!("{}", Local::now())
+    } else {
+        "Unknown mode. Mode available is utc or local".to_string()
+    }
+}
+
 fn index() -> String {
-    format!("Tools:\nRNG: /rand/<min>/<max>")
+    format!("Tools:
+        RNG: /rand/<min>/<max>
+        Now: /now/<utc/local>")
 }
 
 #[tokio::main]
@@ -14,6 +27,10 @@ async fn main() {
     // GET /rand/<min>/<max>
     let rand = warp::path!("rand" / u16 / u16)
         .map(|min, max| random(min, max));
+    // GET /now/<utc/local>
+    let now = warp::path!("now" / String)
+        .map(|mode| right_now(mode));
+    // GET /
     let index = warp::path::end()
         .map(index);
 
@@ -21,6 +38,7 @@ async fn main() {
     let routes = warp::get().and(
         index
             .or(rand)
+            .or(now)
     );
 
     warp::serve(routes)
