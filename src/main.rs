fec73fdafad67232c6f2948b9b70c0742378ab5e
Dang Hoang Tuan <danghoangtuan526@proton.me>
2022-06-27 11:40:58 +0700

    Add code :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+use rand::Rng;
+
+fn random(min: u16, max: u16) -> String {
+    format!("{}", rand::thread_rng().gen_range(min..max))
+}
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let rand = warp::path!("rand" / u16 / u16)
+        .map(|min, max| random(min, max));
+
+    warp::serve(rand)
+        .run(([0, 0, 0, 0], 8000))
+        .await;
 }
